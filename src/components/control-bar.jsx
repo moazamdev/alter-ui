eaae4893b5c208c8bfd255623ed6b296d2a6c7fe
Moazam Ali <mozalikorai@gmail.com>
2023-08-05 23:39:35 +0500

    Updating the control colors with the color picker

--- src/components/control-bar.jsx
+++ src/components/control-bar.jsx
@@ -7,7 +7,6 @@ const ControlBar = () => {
 		<div className="flex justify-center items-center w-full mx-auto my-0 px-2.5 py-[20px] md:py-[50px]">
 			<div className="w-[min(100%_-_15px,1250px)] border border-[#bbb] p-[15px] rounded-lg grid grid-cols-6 gap-[10px] bg-[#ccc] fixed bottom-[10px] drop-shadow-2xl">
 				<ControlButton colorValue={"#010205"} buttonText={"Primary"} />
-
 				<ControlButton
 					colorValue={"#EBEBEB"}
 					buttonText={"Secondary"}
@@ -18,41 +17,7 @@ const ControlBar = () => {
 					colorValue={"#F9F9F9"}
 					buttonText={"Background"}
 				/>
-				<ControlButton
-					colorValue={"#ffffff"}
-					buttonText={"Background"}
-				/>
-
-				{/* <div
-					// onClick={() => alert("hello")}
-					className={`${classes} bg-primary text-white`}
-				>
-					Primary
-					<div className="absolute bottom-[130%] left-0 right-0 w-[300px] h-auto p-5 bg-[#fff] rounded-lg flex justify-center items-center text-[10px] flex flex-col gap-[10px]">
-						<HslaColorPicker />
-						<HexColorInput
-							style={{
-								borderRadius: "10px",
-								width: "60%",
-								height: "3em",
-								background: "#ccc",
-								color: "black",
-								textAlign: "center",
-								fontSize: "16px",
-								outline: "none",
-							}}
-						/>
-					</div>
-				</div> */}
-				{/* <div className={`${classes} bg-secondary text-black`}>
-					Secondary
-				</div>
-				<div className={`${classes} bg-text text-white`}>Text</div>
-				<div className={`${classes} bg-accent text-white`}>Accent</div>
-				<div className={`${classes} bg-mainbg text-black`}>
-					Background
-				</div>
-				<div className={`${classes} bg-white text-black`}>Font</div> */}
+				<ControlButton colorValue={"#ffffff"} buttonText={"Font"} />
 			</div>
 		</div>
 	);
--- src/components/control-button.jsx
+++ src/components/control-button.jsx
@@ -6,12 +6,28 @@ const ControlButton = ({ colorValue, buttonText }) => {
 	console.log("colorValue: ", colorValue);
 	console.log("color: ", color);
 	console.log(typeof color);
+	const getTextColor = (backgroundColor) => {
+		// Convert background color to RGB values
+		const rgb = parseInt(backgroundColor.slice(1), 16);
+		const r = (rgb >> 16) & 0xff;
+		const g = (rgb >> 8) & 0xff;
+		const b = (rgb >> 0) & 0xff;
+
+		// Calculate brightness using luminance formula
+		const brightness = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+		// Return dark or light text color based on brightness
+		return brightness > 0.5 ? "text-black" : "text-white";
+	};
 	return (
 		<div
-			className={`relative font-semibold text-center uppercase tracking-[2px] text-[13px] px-[30px] py-[20px] rounded-lg select-none cursor-pointer text-white bg-[${color}]`}
+			className={`relative font-semibold text-center uppercase tracking-[2px] text-[13px] px-[30px] py-[20px] rounded-lg select-none cursor-pointer ${getTextColor(
+				color
+			)}`}
+			style={{ background: color }}
 		>
 			{buttonText}
-			<div className="absolute bottom-[130%] left-0 right-0 w-fit h-auto p-4 bg-[#fff] rounded-lg flex justify-center items-center text-[10px] flex flex-col gap-[10px] cursor-default">
+			{/* <div className="absolute bottom-[130%] left-0 right-0 w-fit h-auto p-4 bg-[#fff] rounded-lg flex justify-center items-center text-[10px] flex flex-col gap-[10px] cursor-default">
 				<HexColorPicker color={color} onChange={setColor} />
 				<HexColorInput
 					color={color}
@@ -27,7 +43,7 @@ const ControlButton = ({ colorValue, buttonText }) => {
 						outline: "none",
 					}}
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 };
